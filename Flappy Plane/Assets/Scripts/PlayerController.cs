37a563673edb76971a3456ae922c2940c7dc97b7
Victor Real <victor.realchisto30@hotmail.com>
2024-02-18 22:24:52 -0300

    player fall speed limited

--- Flappy Plane/Assets/Scripts/PlayerController.cs
+++ Flappy Plane/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 {
     //Criando uma variável para acessar as propriedades de RigidBody
     private Rigidbody2D PlayerRB;
+
     private bool isSpacePressed;
     private float playerSpeed;
 
@@ -15,6 +16,8 @@ public class PlayerController : MonoBehaviour
     {
         //Pegando o RigidBody do componente 
         PlayerRB = GetComponent<Rigidbody2D>();
+
+        //Definindo velocidade do Player
         playerSpeed = 4f;
     }
 
@@ -23,9 +26,18 @@ public class PlayerController : MonoBehaviour
     {
         //Checando se espaço está pressionado
         isSpacePressed = Input.GetKeyDown(KeyCode.Space);  
+
+        //Executando pulo se espaço for pressionado
         if (isSpacePressed)
-        {
+        {   
+            //Definindo velocidade do rigidbody para cima e multiplicando pela velocidade determinada ao player
             PlayerRB.velocity = Vector2.up * playerSpeed;
+        }
+
+        //Limitando a velocidade de queda do Player
+        if(PlayerRB.velocity.y < -playerSpeed)
+        {
+            PlayerRB.velocity = Vector2.down * playerSpeed;
         }
     }
 }
