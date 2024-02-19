36f30f0dd8da1d2f6bcf66a5bb8f8fddd4ee3ded
Victor Real <victor.realchisto30@hotmail.com>
2024-02-18 22:16:37 -0300

    player jump control created

--- Flappy Plane/Assets/Scripts/PlayerController.cs
+++ Flappy Plane/Assets/Scripts/PlayerController.cs
@@ -1,22 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     //Criando uma variável para acessar as propriedades de RigidBody
-    private Rigidbody2D rigidbody;
+    private Rigidbody2D PlayerRB;
+    private bool isSpacePressed;
+    private float playerSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         //Pegando o RigidBody do componente 
-        rigidbody = GetComponent<Rigidbody2D>();
+        PlayerRB = GetComponent<Rigidbody2D>();
+        playerSpeed = 4f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Checando se espaço está pressionado
+        isSpacePressed = Input.GetKeyDown(KeyCode.Space);  
+        if (isSpacePressed)
+        {
+            PlayerRB.velocity = Vector2.up * playerSpeed;
+        }
     }
 }
