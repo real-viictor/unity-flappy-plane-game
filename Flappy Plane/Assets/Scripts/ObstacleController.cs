46c0918340e84cebe0a8fe6a5b985ba3f02d0c43
Victor Real <victor.realchisto30@hotmail.com>
2024-02-19 21:18:20 -0300

    refactored obstacle movement

--- Flappy Plane/Assets/Scripts/ObstacleController.cs
+++ Flappy Plane/Assets/Scripts/ObstacleController.cs
@@ -5,17 +5,20 @@ using UnityEngine.UIElements;
 
 public class ObstacleController : MonoBehaviour
 {
+    private float obstacleSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
         //Definindo posição inicial do obstáculo, o colocando fora da tela e determinando altura
         transform.position = new Vector3(10f, Random.Range(-2.25f, 0.9f), 0f);
+        obstacleSpeed = 0.004f;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Determinando que o objeto se mova para a esquerda
-        transform.position -= new Vector3(0.005f, 0, 0);
+        transform.position += Vector3.left * obstacleSpeed;
     }
 }
