145f0746417dead8d3dd166f998d8777987cda10
Victor Real <victor.realchisto30@hotmail.com>
2024-02-19 21:23:12 -0300

    equalizing obstacle speed per frame with deltaTime

--- Flappy Plane/Assets/Scripts/ObstacleController.cs
+++ Flappy Plane/Assets/Scripts/ObstacleController.cs
@@ -11,14 +11,16 @@ public class ObstacleController : MonoBehaviour
     void Start()
     {
         //Definindo posição inicial do obstáculo, o colocando fora da tela e determinando altura
-        transform.position = new Vector3(10f, Random.Range(-2.25f, 0.9f), 0f);
-        obstacleSpeed = 0.004f;
+        transform.position = new Vector3(10f, Random.Range(-2.25f, 0.9f), 0f); 
+
+        //Determinando velocidade do obstáculo e multiplicando por DeltaTime para ajustar a velocidade para todos os frames do jogo
+        obstacleSpeed = 0.15f * Time.deltaTime;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Determinando que o objeto se mova para a esquerda
-        transform.position += Vector3.left * obstacleSpeed;
+        transform.position += Vector3.left * obstacleSpeed; 
     }
 }
