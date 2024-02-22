3cd26efb16ee7ffd0c6fb56520b4ff172a5f8810
Victor Real <victor.realchisto30@hotmail.com>
2024-02-21 22:54:02 -0300

    game controller timer defined

--- Flappy Plane/Assets/Scripts/GameController.cs
+++ Flappy Plane/Assets/Scripts/GameController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    //Criando variável de timer e definindo valor para ele
+    private float timer = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,22 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        AtivarTimer();
+    }
+
+    //Dando um trigger para o Timer
+    void AtivarTimer()
+    {
+        if (timer > 0f)
+        {
+            //Se timer não for 0 ou menor, reduza o valor do timer por deltaTime (O tempo que se passou entre um frame e outro)
+            //Esta operação faz com que cada 1f seja 1 segundo, portanto 2f = 2s, 3f = 3s etc...
+            timer -= Time.deltaTime;
+        } else
+        {
+            //Caso contrário, redefina o timer e execute uma ação
+            timer = 1f;
+            Debug.Log("oi");
+        }
     }
 }
